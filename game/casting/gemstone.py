12c53dcf39afe8a38d318ebcf4504f8487b3020f
djsevy <dallinsevy@gmail.com>
2022-03-17 14:02:38 -0600

    changes to gemsgtone and player

---
+++ game/casting/gemstone.py
@@ -0,0 +1,56 @@
+from game.casting.player import Player
+
+
+class Gemstone(Player):
+    """
+    The gems and the stones 
+    
+    The responsibility of an Gemstones is to move both the gems, the stones, and their vales 
+
+    Attributes:
+        gem_value, stone_value
+    """
+    def __init__(self):
+        super().__init__()
+        self._pos_value = 100
+        self._neg_value = -100
+        self._pos_message = "100"
+        self._neg_message = "-100"
+        
+    def get_gem_value(self):
+        """Gets the artifact's message.
+        
+        Returns:
+            string: The message.
+        """
+
+        
+        return self._pos_value
+    
+    def get_stone_value(self):
+        
+        return self._neg_value
+    
+
+    def adjust_value(self, _neg_message, _pos_message):
+        
+        
+        
+       
+        for value in self._pos_message and self._neg_message:
+            if Player.get_position() == _pos_message:
+                return self._pos_value
+            
+            elif Player.get_position() == _neg_message:
+                return self._neg_value
+
+
+
+
+    # def set_pos_value(self, ):
+    #     """Updates the message to the given one.
+        
+    #     Args:
+    #         message (string): The given message.
+    #     """
+    #     self._message = message
